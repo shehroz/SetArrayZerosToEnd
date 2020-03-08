dc288ae8c46c0a673c54e4cddd242b68749d825c
shehroz <shehrozkhan91@hotmail.com>
2020-03-08 17:22:16 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace Algorithms
 
             int[] arr = new int[] { 1, 2, 3, 4, 0, 5, 6, 7, 0, 8, 0 };
             int[] result = SetZeroToEnd(arr);
+            // int[] result = SetZeroToStart(arr);
             for (int i = 0; i < result.Length; i++)
             {
                 Console.WriteLine(result[i]);
@@ -23,19 +24,19 @@ namespace Algorithms
 
         public static int[] SetZeroToEnd(int[] arr)
         {
-            int index = 0, count = 0;
-            for (int i = 0; i < arr.Length; i++)
+            int count = 0;
+            for (int i = 0; i < arr.Length - count; i++)
             {
-                if (arr[i] != 0)
-                    arr[index++] = arr[i];
-                else
+                if(arr[i]==0)
+                {
+                    for (int j = i; j < arr.Length-1; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = 0;
+                    }
                     count++;
+                }
             }
-            for (int i = arr.Length - 1; i > arr.Length - count; i--)
-            {
-                arr[i] = 0;
-            }
-
             return arr;
         }
         public static int[] SetZeroToStart(int[] arr)
